68870be9ddb0a2e9f7cfa6ae0a10a876464f4262
sachinchauhan010 <010sssachin@gmail.com>
2024-07-15 08:48:31 +0530

    project setup and side nav ui done

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,11 @@
+'use client'
+
+function Dashborad() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Dashborad
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/ThemeProvider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,8 +12,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider></body>
     </html>
   );
 }
